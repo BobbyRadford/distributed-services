bd99cefa72e74a155ae11d5f9ee5edf95337b293
krkantes <krkantes@ncsu.edu>
2017-03-16 13:41:56 -0400

    fixed run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
 @echo off
 start "number-gen-service" java -jar number-gen-service\target\number-gen-service-1.0.0.jar server number-gen-service\number-gen-service.yml
 start "sleep-service"      java -jar sleep-service\target\sleep-service-1.0.0.jar           server sleep-service\sleep-service.yml
-pause
